f7e28d5ed2e1704cd58b9ca3b9a980644db26d53
Brian Snook <1421890+bmsnook@users.noreply.github.com>
2025-08-10 23:14:16 -0400

    standardized indentation on 4 spaces

--- lambda_function_signed_s3.py
+++ lambda_function_signed_s3.py
@@ -6,18 +6,27 @@ bucket_name = 'bms-gm-pdfs'
 object_key = 'lorem_ipsum.pdf'
 expiration_seconds = 600
 
+test_input = "{'key1': 'value1', 'key2': 'value2', 'key3': 'value3'}"
+test_context = "{}"
+
 def lambda_handler(event, context):
     """
     Lambda handler function.
     """
 
-  presigned_url = s3_client.generate_presigned_url(
-    ClientMethod='get_object',
-    Params={'Bucket': bucket_name, 'Key': object_key},
-    ExpiresIn=expiration_seconds
-  )
+    presigned_url = s3_client.generate_presigned_url(
+        ClientMethod='get_object',
+        Params={'Bucket': bucket_name, 'Key': object_key},
+        ExpiresIn=expiration_seconds
+    )
 
-  return {
-    'statusCode': 200,
-    'body': presigned_url
-  }
+    print(presigned_url)
+
+    return {
+        'statusCode': 200,
+        'body': presigned_url
+    }
+
+
+if __name__ == "__main__":
+    lambda_handler(test_input,test_context)
